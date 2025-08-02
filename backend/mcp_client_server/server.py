a995bfffebb17677f7b4c4e0dbb3a5c4cebb543e
Gael <gaelgilbello27@gmail.com>
2025-08-01 20:35:21 -0700

    updated server tool

--- backend/mcp_client_server/server.py
+++ backend/mcp_client_server/server.py
@@ -1,15 +1,11 @@
 from mcp.server.fastmcp import FastMCP
 from mcp.server.fastmcp.utilities.logging import get_logger
-from openai import OpenAI
 from dotenv import load_dotenv
 from pathlib import Path
-import os
+
 
 load_dotenv(Path("../.env"))
 
-
-ARXIV_NAMESPACE = "{http://www.w3.org/2005/Atom}"
-LLM = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 logger = get_logger(__name__)
 
@@ -25,7 +21,7 @@ mcp = FastMCP(
     name="search_event_brite_events",
     description="Search for events on eventbrite on a selected month",
 )
-def search_event_brite_events(month: str) -> list[dict]:
+def search_event_brite_events(month: str, location: str) -> list[dict]:
     pass
 
 
@@ -33,13 +29,13 @@ def search_event_brite_events(month: str) -> list[dict]:
     name="search_luma_events",
     description="Search for events on eventbrite on a selected month",
 )
-def search_luma_events(month) -> list[dict]:
+def search_luma_events(month: str, location: str) -> list[dict]:
     pass
 
 
 @mcp.tool(
-    name="save_txt",
-    description="Save text to a .txt file",
+    name="analyze_events",
+    description="Analyzes the provided events to see which are the most relevant",
 )
 def analyze_events(events: list[dict]) -> list[dict]:
     """Saves the provided text to a .txt file."""
@@ -47,34 +43,29 @@ def analyze_events(events: list[dict]) -> list[dict]:
 
 
 @mcp.tool(
-    name="writer_tool",
-    description="Writes an essay on a given topic",
+    name="analyze_calender",
+    description="Analyze the events on the google calendar",
 )
-def writer_tool(query: str, context: str) -> str:
+def analyze_calendar(calendar: list[dict]) -> str:
     """"""
     pass
 
 
 @mcp.tool(
-    name="review_tool",
-    description="Reviews content on a given topic",
+    name="create_google_event",
+    description="Create an event on google calendar",
 )
-def review_tool(content: str, context: str) -> str:
+def create_google_event(event: dict) -> str:
     """"""
     pass
 
 
 @mcp.tool(
-    name="assemble_content",
-    description="Assamble content from previous tools (context) and current content state",
+    name="update_calendar_event",
+    description="Update an event on google calendar",
 )
-def assemble_content(content: str, context: str) -> str:
+def assemble_content(calendar: list[dict]) -> str:
     """"""
-    pass
-
-
-@mcp.tool(name="arxiv_search", description="Search arxiv")
-def arxiv_search(query: str) -> str:
     pass
 
 
