7135c409e046ca7cf077ec4822dfbee4887fa609
Gael <gaelgilbello27@gmail.com>
2025-08-05 23:05:47 -0700

    tried out sample

--- backend/sample.py
+++ backend/sample.py
@@ -19,14 +19,16 @@ def main():
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists("token.json"):
-        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    if os.path.exists("./token.json"):
+        creds = Credentials.from_authorized_user_file("./token.json", SCOPES)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file(
+                "./credentials.json", SCOPES
+            )
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open("token.json", "w") as token:
@@ -43,7 +45,7 @@ def main():
             .list(
                 calendarId="primary",
                 timeMin=now,
-                maxResults=10,
+                maxResults=30,
                 singleEvents=True,
                 orderBy="startTime",
             )
