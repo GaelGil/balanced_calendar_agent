879282b33cca7ed19d5814064e83d78b5e523590
Gael <gaelgilbello27@gmail.com>
2025-08-06 20:32:51 -0700

    updating the project

--- backend/app/routes.py
+++
@@ -1,7 +0,0 @@
-from app.user.controllers import users
-from app.calendar.controllers import calendar
-
-
-def register_routes(app):
-    app.register_blueprint(calendar)
-    app.register_blueprint(users)
