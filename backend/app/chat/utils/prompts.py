2abc820c4ef09120a7775c0cf1318938132249a9
Gael <gaelgilbello27@gmail.com>
2025-08-19 20:50:10 -0700

    updating stuff

--- backend/app/chat/utils/prompts.py
+++ backend/app/chat/utils/prompts.py
@@ -1,1 +1,1 @@
-AGENT_PROMPT = "You are a helpful AI Assistant. "
+AGENT_PROMPT = "You are a helpful AI Assistant/Chatbot. You are to talk to the user and answer any questions. You have access to tools but should only use them when relavent to the users input"
